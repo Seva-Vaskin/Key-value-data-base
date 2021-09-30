57d7a06e30a1a8964aed67d1818a23999e69d867
seva-vaskin <vaskin.2003@gmail.com>
2021-09-30 18:02:13 +0300

    created DataBase class

---
+++ src/main/kotlin/DataBase.kt
@@ -0,0 +1,22 @@
+class DataBase(val path: String = "") {
+
+    fun exists(): Boolean {
+        TODO()
+    }
+
+    fun fetchKey(key: String) {
+        TODO()
+    }
+
+    fun deleteKey(key: String) {
+        TODO()
+    }
+
+    fun create() {
+        TODO()
+    }
+
+    fun delete() {
+        TODO()
+    }
+}
