4c2b7ff9d2a7c3aaf6846f9c4a4dc27e45768d7c
seva-vaskin <vaskin.2003@gmail.com>
2021-09-30 19:00:48 +0300

    refactoring: constant was renamed

--- src/main/kotlin/Hashing.kt
+++ src/main/kotlin/Hashing.kt
@@ -1,10 +1,10 @@
-const val primeNumber = 257L
+const val MultiplyNumber = 257L
 const val ModuloNumber = 1009L
 
 fun getHash(key: String): Int {
     var hash = 0
     key.forEach {
-        hash = ((hash.toLong() * primeNumber + it.code) % ModuloNumber).toInt()
+        hash = ((hash.toLong() * MultiplyNumber + it.code) % ModuloNumber).toInt()
     }
     return hash
 }
