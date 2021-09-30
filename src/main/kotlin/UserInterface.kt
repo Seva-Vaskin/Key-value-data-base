5b2e783c0de915c59571bcffb93aa26ac1dd4ff0
seva-vaskin <vaskin.2003@gmail.com>
2021-09-30 17:56:11 +0300

    refactoring: edited commands list

--- src/main/kotlin/UserInterface.kt
+++ src/main/kotlin/UserInterface.kt
@@ -1,12 +1,11 @@
 enum class Commands(val str: String) {
     Quit("quit"),
-    Fetch("fetch"),
+    FetchKey("fetch"),
     DeleteKey("delete"),
     Store("store"),
     CreateNewDataBase("newdb"),
     DeleteDataBase("deldb"),
     OpenDataBase("open"),
-    CloseDataBase("close"),
 }
 
 fun processCommand(command: Commands) {
